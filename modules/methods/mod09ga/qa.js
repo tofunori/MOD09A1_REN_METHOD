9a3d31aeb1b237d5ffc89fc32ca0799c170ac147
Tofunori <tofunori@example.com>
2025-07-01 17:37:22 -0400

    commit cloud

--- modules/methods/mod09ga/qa.js
+++ modules/methods/mod09ga/qa.js
@@ -5,9 +5,11 @@
 
 function qualityFilter(image) {
   var qa = image.select('state_1km');
-  var clearSky = qa.bitwiseAnd(0x3).lte(1);
+  // Bits 0-1 (cloud state): 00 = clear.  Keep only fully clear pixels.
+  var clearSky = qa.bitwiseAnd(0x3).eq(0);
   var shadowFree = qa.bitwiseAnd(1 << 2).eq(0);
-  var noCirrus = qa.bitwiseAnd(1 << 8).lte(1);
+  // Bits 8-9 (cirrus detected): 00 = none.  Reject any cirrus contamination.
+  var noCirrus = qa.bitwiseAnd(3 << 8).eq(0);
   var snowIceConf = qa.bitwiseAnd(0x3000).rightShift(12);
   var validSnowIce = snowIceConf.gt(0);
   var solarZenith = image.select('SolarZenith').multiply(0.01);
